86825c2b4336e6e0d2a565520188d4b40d2452cc
AutomaticNBA <automaticnba@gmail.com>
2022-09-09 18:44:21 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 #from streamlit_option_menu import option_menu
-from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
+#from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 from PIL import Image
 
 st.set_page_config(
