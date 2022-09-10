88cb3009e495a82e749f334b3d9f5ab36a4458ae
AutomaticNBA <automaticnba@gmail.com>
2022-09-09 22:28:05 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -75,7 +75,10 @@ if selected == "Create Player Charts":
 
     year = st.selectbox(
         'Select A Season',
-        ('Every Season', year_options, 0)
+        ('Every Season', '2022', '2021', '2020', '2019', '2018', '2017',
+         '2016', '2015', '2014', '2013', '2012', '2011', '2010', '2009',
+         '2008', '2007', '2006', '2005', '2004', '2003', '2002', '2001',
+         '2000', '1999', '1998', '1997')
     )
 
     cdf1 = cdf[cdf['Year'] == year]
