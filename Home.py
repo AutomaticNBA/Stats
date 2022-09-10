0578b8eabfb8dd6231eb8f18d8024e4dc2c84238
AutomaticNBA <automaticnba@gmail.com>
2022-09-10 12:35:19 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -106,7 +106,8 @@ if selected == "Create Player Charts":
             if p2 != "None":
                 cdf2 = cdf[(cdf['PlayerSansYear'] == p2)]
 
-        cdf = [cdf1, cdf2]
+        pla = [cdf1, cdf2]
+        cdf = pd.concat(pla)
 
 
             #cdf = cdf[cdf['PlayerSansYear'] == p1,p2]
