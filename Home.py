e299c75a18ef1220efcc6059af33a2999a05434a
AutomaticNBA <automaticnba@gmail.com>
2022-09-09 22:27:13 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -75,11 +75,10 @@ if selected == "Create Player Charts":
 
     year = st.selectbox(
         'Select A Season',
-        ('Every Season', '2022', '2021', '2020', '2019', '2018', '2017',
-         '2016', '2015', '2014', '2013', '2012', '2011', '2010', '2009',
-         '2008', '2007', '2006', '2005', '2004', '2003', '2002', '2001',
-         '2000', '1999', '1998', '1997')
+        ('Every Season', year_options, 0)
     )
+
+    cdf1 = cdf[cdf['Year'] == year]
 
     st.write("Double click to reset chart")
 
@@ -96,14 +95,13 @@ if selected == "Create Player Charts":
             st.plotly_chart(plots)
 
     else:
-        cdf = cdf[cdf['Year']==year]
 
-        plots = px.scatter(cdf, x=x_axis_val, y=y_axis_val, hover_name=cdf.Player, hover_data=['GP'],
+        plots = px.scatter(cdf1, x=x_axis_val, y=y_axis_val, hover_name=cdf1.Player, hover_data=['GP'],
                            title=(year + ' ' + x_axis_val + ' ' + 'vs' + ' ' + y_axis_val))
 
         if st.checkbox('Plot Names'):
-            plots = px.scatter(cdf, x=x_axis_val, y=y_axis_val, hover_name=cdf.Player, hover_data=['GP'],
-                               title=(year + ' ' + x_axis_val + ' ' + 'vs' + ' ' + y_axis_val), text=cdf.Player)
+            plots = px.scatter(cdf1, x=x_axis_val, y=y_axis_val, hover_name=cdf1.Player, hover_data=['GP'],
+                               title=(year + ' ' + x_axis_val + ' ' + 'vs' + ' ' + y_axis_val), text=cdf1.Player)
 
         if st.button('Plot Chart'):
             st.plotly_chart(plots)
