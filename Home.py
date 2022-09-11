f7693a761cac34585ecc7bf64031b9f0982a50c2
AutomaticNBA <automaticnba@gmail.com>
2022-09-11 02:36:19 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -171,7 +171,7 @@ if selected == "Create Player Charts":
         cdf = cdf[cdf['GP'] >= min_gp]
 
 
-        min = st.slider("Minimum Minutes Played", 0, 4500,10)
+        min = st.slider("Minimum Minutes Played", 0, 4500,100)
 
         cdf = cdf[cdf['MIN'] >= min ]
 
